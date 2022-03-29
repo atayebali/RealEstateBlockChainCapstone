f709c3864e7e907741d9253f4ca41fbe12136514
Anis Tayebali <atayebali@gmail.com>
2022-03-29 03:52:19 +0000

    Tests getting done.

--- eth-contracts/test/TestERC721Mintable.js
+++ eth-contracts/test/TestERC721Mintable.js
@@ -23,11 +23,17 @@ contract('TestERC721Mintable', accounts => {
 
         // token uri should be complete i.e: https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/1
         it('should return token uri', async function () {
-
+            let tokenuri = await this.contract.tokenURI(1)
+            assert(tokenuri, "https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/1", "URI is incorrect")
         })
 
         it('should transfer token from one owner to another', async function () {
+            await this.contract.safeTransferFrom(account_one, account_two, 1)
+            var balance = await this.contract.balanceOf.call(account_one)
+            assert(balance, 0, "Balance is incorrect ")
 
+            var balance = await this.contract.balanceOf.call(account_two)
+            assert(balance, 1, "Balance is incorrect ")
         })
     });
 
@@ -37,7 +43,12 @@ contract('TestERC721Mintable', accounts => {
         })
 
         it('should fail when minting when address is not contract owner', async function () {
-
+            try {
+                await this.contract.mint(account_two, 1, { from: account_two })
+            } catch (e) {
+                var message = "Returned error: VM Exception while processing transaction: revert Must be owner to execute -- Reason given: Must be owner to execute."
+                assert(e, message, "Expected an erorr but nothing happened.")
+            }
         })
 
         it('should return contract owner', async function () {
